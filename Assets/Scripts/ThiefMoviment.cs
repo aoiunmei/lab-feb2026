d84e7aaacddf6acad4fb97f117993ee72203bfdf
Rafael Moraes <rafaelnec@gmail.com>
2026-02-26 04:15:14 -0800

    chore: add thief moviment

--- Assets/Scripts/ThiefMoviment.cs
+++ Assets/Scripts/ThiefMoviment.cs
@@ -5,12 +5,15 @@ public class ThiefMoviment : MonoBehaviour
 
     [SerializeField] private GameObject thiefInstance;
     [SerializeField] private Transform thiefSpawnPoint;
-    [SerializeField] private float maxY = 5f;
-    [SerializeField] private float minY = -5f;
+    [SerializeField] private Transform targetPosition;
+    [SerializeField] private float maxZ = 5f;
+    [SerializeField] private float minZ = -5f;
+    
     [SerializeField] private float maxX = 5f;
-    [SerializeField] private float minX = -5f;
 
     public static ThiefMoviment Instance { get; private set; }
+
+    public float speed = 5.0f; 
 
     private void Awake()
     {
@@ -25,12 +28,24 @@ public class ThiefMoviment : MonoBehaviour
     }
 
     // Update is called once per frame
-    public void MoveThief()
+
+    public void MoveTowardsTargetAndClamp()
     {
-        Vector3 newPosition = thiefInstance.transform.position;
-        newPosition.x =  Random.Range(minX, maxX);
-        newPosition.y = Mathf.Clamp(newPosition.y, minY, maxY);
-        thiefInstance.transform.position = newPosition;
+        
+        float step = speed * Time.deltaTime;
+
+        Vector3 nextPosition = Vector3.MoveTowards(thiefSpawnPoint.position, targetPosition.position, step);
+
+
+        nextPosition.x -= Random.Range(0, maxX);
+        nextPosition.z = Random.Range(minZ, maxZ); 
+
+        transform.position = nextPosition;
+
+        if (thiefSpawnPoint.position.x <= targetPosition.position.x)
+        {
+            Debug.Log("Thief reached the target position!");
+        }
     }
 
 }
